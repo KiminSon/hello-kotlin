e833fdd192ba36eefa0a16cddb0268c456bfcdf0
KiminSon <kimin0331@naver.com>
2025-12-01 13:54:52 +0900

    1.2. 변수 알아보기

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,16 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    val s = "ABC"
+    val i = 1
+    val l = 1L
+    val d = 1.0
+    val f = 1.0f
+
+    println("s = " + s::class)
+    println("i = " + i::class)
+    println("l = " + l::class)
+    println("d = " + d::class)
+    println("f = " + f::class)
 }
