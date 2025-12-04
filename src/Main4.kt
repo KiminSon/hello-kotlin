260f3ef2ae73f41ad3272cda7b4702ef6a9c08a1
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-12-04 20:38:07 +0900

    1.5. class 와 Interface 알아보기

---
+++ src/Main4.kt
@@ -0,0 +1,26 @@
+fun main() {
+    val item = Item("Book", 10000)
+    println("Item name is ${item.name}, price is ${item.price}")
+
+    item.buy()
+    item.sell()
+}
+
+class Item(
+    val name: String,
+    val price: Int
+) : ItemTrade {
+    override fun buy() {
+        println("[buy] $name")
+    }
+
+    override fun sell() {
+        println("[sell] $name")
+    }
+
+}
+
+interface ItemTrade {
+    fun buy()
+    fun sell()
+}
